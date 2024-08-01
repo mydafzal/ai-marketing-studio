6d2b1c06aba98914c13cb89bbd869a188fdfef13
mydafzal <momalafzal20@gmail.com>
2024-08-01 11:14:23 +0200

    new mapped chat viola sander

--- lib/api/fasty-bot/helpers/campaign-id-list.ts
+++ lib/api/fasty-bot/helpers/campaign-id-list.ts
@@ -1,6 +1,6 @@
 // todo: use database to store chat to campaign id mappings
 const chatToCampaignMapping: Record<string, string> = {
-    "cDSdkK5": "120209736792950390", //vio
+    "g4LRS87": "120209736792950390", //vio
     "shP95xm": "120209736792950390", //vio
     "wXkgJWu": "120209736792950390", //vio
     "3TD0PyU": "120210023478820738", //hyp
