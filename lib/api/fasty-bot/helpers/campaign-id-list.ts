43be4f2e872567eca3af06be2b63780a316cd2e8
mydafzal <momalafzal20@gmail.com>
2024-08-03 10:18:17 +0200

    [TEQ] link demo chat id with campaign

--- lib/api/fasty-bot/helpers/campaign-id-list.ts
+++ lib/api/fasty-bot/helpers/campaign-id-list.ts
@@ -5,6 +5,11 @@ const chatToCampaignMapping: Record<string, string> = {
     "wXkgJWu": "120209736792950390", //vio
     "3TD0PyU": "120210023478820738", //hyp
     "8526ZJW": "120210023478820738", //hyp
+    "vgg7N3L": "23856096985670148", //Demo-campaign
+    "G0s9KMI": "23856096985670148", //Demo-campaign
+    "AS5xryw": "23856096985670148", //Demo-campaign
+    "4u4YJds": "23856096985670148", //Demo-campaign
+    "d83tZY7": "23856096985670148", //Demo-campaign
     "DBiGirw": "123123123123"
     // Add more mappings as needed
 };
