fcc5caf8a16779675f77fedab5d627c7c1ddb7c3
mydafzal <momalafzal20@gmail.com>
2024-08-17 18:16:39 +0200

    add basic authentication on /admin

--- middleware.ts
+++ middleware.ts
@@ -5,25 +5,35 @@ import type { NextRequest } from 'next/server'
 
 const nextAuthMiddleware = NextAuth(authConfig).auth
 
+function basicAuth(request: NextRequest) {
+  const authHeader = request.headers.get('authorization')
+
+  if (authHeader) {
+    const auth = authHeader.split(' ')[1]
+    const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
+
+    const [expectedUser, expectedPwd] = process.env.FEATURE_TOGGLE_CREDS?.split(':') || []
+
+    if (user === expectedUser && pwd === expectedPwd) {
+      return true
+    }
+  }
+
+  return false
+}
+
 async function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === '/feature-toggles' || request.nextUrl.pathname === '/feature-toggles/demo') {
-    const authHeader = request.headers.get('authorization')
+  const protectedRoutes = ['/feature-toggles', '/feature-toggles/demo', '/admin']
 
-    if (authHeader) {
-      const auth = authHeader.split(' ')[1]
-      const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
-
-      const [expectedUser, expectedPwd] = process.env.FEATURE_TOGGLE_CREDS?.split(':') || []
-
-      if (user === expectedUser && pwd === expectedPwd) {
-        return NextResponse.next()
-      }
+  if (protectedRoutes.some(route => request.nextUrl.pathname.startsWith(route))) {
+    if (basicAuth(request)) {
+      return NextResponse.next()
     }
 
     return new NextResponse('Authentication required', {
       status: 401,
       headers: {
-        'WWW-Authenticate': 'Basic realm="Feature Toggles"',
+        'WWW-Authenticate': 'Basic realm="Protected Area"',
       },
     })
   }
