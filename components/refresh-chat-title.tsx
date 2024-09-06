f99538c24899a5739c73197829fb99f5ae3b0685
mydafzal <momalafzal20@gmail.com>
2024-09-06 19:12:51 +0200

    fix RefreshChatTitle component

--- components/refresh-chat-title.tsx
+++ components/refresh-chat-title.tsx
@@ -1,9 +1,18 @@
 'use client'
 import { useEffect } from 'react'
 
-export const RefreshChatTitle = () => {
+export const RefreshChatTitle = ({ campaignId, campaignName }: { campaignId: string, campaignName: string }) => {
     useEffect(() => {
-        window.dispatchEvent(new CustomEvent("update-chat-title"))
+        window.dispatchEvent(
+            new CustomEvent(
+                "update-chat-title", {
+                    detail: {
+                        campaignId,
+                        campaignName
+                    }
+                }
+            )
+        )
     }, [])
 
     return <></>
