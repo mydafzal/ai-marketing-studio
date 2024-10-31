72eeead999eb06d4e583d4c260935314ecfcab3e
mydafzal <momalafzal20@gmail.com>
2024-10-31 09:56:54 +0100

    fix TS issue

--- components/prompt-form.tsx
+++ components/prompt-form.tsx
@@ -3,7 +3,7 @@
 import * as React from 'react'
 
 import Textarea from 'react-textarea-autosize'
-import { FilePart, ImagePart, TextPart, UserContent } from 'ai'
+import { ImagePart, TextPart, UserContent } from 'ai'
 import chatToCampaignMapping from '@/lib/api/fasty-bot/helpers/campaign-id-list'
 import { Button } from '@/components/ui/button'
 import { IconArrowElbow, IconPlus, IconSpinner } from '@/components/ui/icons'
@@ -22,7 +22,7 @@ import { Message } from '@/lib/types'
 import { getMimeType } from '@/lib/utils'
 
 export interface PromtFormProps {
-  onSendMessage: (message: string, userContent?: (TextPart | ImagePart | FilePart)[]) => Promise<void>
+  onSendMessage: (message: string, userContent?: (TextPart | ImagePart )[]) => Promise<void>
 }
 const MAX_IMAGE_SIZE = 4 * 1024 * 1024;
 const MAX_VIDEO_SIZE = 1 * 1024 * 1024 * 1024;
