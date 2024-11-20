8e9071e7426380c1daeefbfc4d9b7d2045485f9a
mydafzal <momalafzal20@gmail.com>
2024-11-20 23:52:31 +0530

    feat: select multiple countries in geo-graphical-locations

--- components/geographical-location.tsx
+++ components/geographical-location.tsx
@@ -36,6 +36,14 @@ interface GeoGraphicalLocationProps {
   isReadOnly?: boolean
 }
 
+type GeoLocaion={
+    country: Country|null;
+    region: Region|null;
+    cities: City[];
+    regionData:Region[];
+    cityData:City[]
+}
+
 export function GeographicalLocation({
   toolCallId,
   uiProps,
@@ -46,6 +54,7 @@ export function GeographicalLocation({
   const { confirmUpdateAdset, submitUserMessage, syncMessages } = useActions()
   const [aiState, setAIState] = useAIState()
   const [_, setMessages] = useUIState<typeof AI>()
+
   const [countryData, setCountryData] = useState<Country[]>([])
   const [countrySelected, setCountrySelected] = useState<Country>()
   const [regionData, setRegionData] = useState<Region[]>([])
@@ -53,6 +62,17 @@ export function GeographicalLocation({
   const [loading, setLoading] = useState(false)
   const [cityData, setCityData] = useState<City[]>([])
   const [citiesSelected, setCitiesSelected] = useState<City[]>([])
+
+  const [selectedGeoLocations, setSelectedGeoLocations] = useState<GeoLocaion[]>([
+    {
+      country:null,
+      region:null,
+      cities:[],
+      regionData:[],
+      cityData:[]
+    }
+  ])
+
   const [isMale, setIsMale] = useState<boolean>(false)
   const [isFemale, setIsFemale] = useState<boolean>(false)
 
@@ -70,7 +90,7 @@ export function GeographicalLocation({
     if (!adset) {
       return toast.info('Adset is not set in this chat, please select adset first.')
     }
-    if (!countrySelected) {
+    if (!selectedGeoLocations[0].country) {
       return toast.info('You should select at least a country to set targeting.')
     }
     setIsSubmitting(true)
@@ -93,24 +113,41 @@ export function GeographicalLocation({
     newTargeting.age_max = ageMax
     newTargeting.genders = genders
 
-    if (citiesSelected.length > 0) {
+    if (
+      selectedGeoLocations.some((location) => location.cities.length > 0)
+    ) {
       newTargeting.geo_locations = {
-        cities: citiesSelected.map(city => ({
-          key: city.key
-        }))
-      }
-
-      demographicData.cities = citiesSelected
-    } else if (regionSelected) {
+        cities: selectedGeoLocations
+          .flatMap((location) => location.cities) 
+          .map((city) => ({ key: city.key })),
+      };
+    
+      demographicData.cities = selectedGeoLocations.flatMap(
+        (location) => location.cities 
+      );
+    }else if (regionSelected) {
       newTargeting.geo_locations = {
-        regions: [{ key: regionSelected?.key }]
-      }
-      demographicData.regions = [regionSelected]
+        regions: selectedGeoLocations
+          .map((location) => location.region?.key)
+          .filter((regionKey): regionKey is string => !!regionKey) 
+          .map((key) => ({ key })), 
+      };
+    
+      demographicData.regions = selectedGeoLocations
+        .map((location) => location.region)
+        .filter((region): region is Region => !!region); 
     } else {
       newTargeting.geo_locations = {
-        countries: [countrySelected?.country_code]
-      }
-      demographicData.countries = [countrySelected]
+        countries: selectedGeoLocations
+          .map((location) => location.country?.country_code)
+          .filter((countryCode): countryCode is string => !!countryCode),
+      };
+      
+      
+      
+      demographicData.countries = selectedGeoLocations
+      .map((location) => location.country?.country_code)
+      .filter((countryCode): countryCode is string => !!countryCode)
     }
     demographicData.age_min = ageMin
     demographicData.age_max = ageMax
@@ -192,22 +229,24 @@ export function GeographicalLocation({
         console.error('Error fetching:', error)
       })
   }
-  const getRegionList = (countryCode: string) => {
+  const getRegionList = async (countryCode: string): Promise<Region[]> => {
     const params = {
       type: 'adgeolocation',
       location_types: "['region']",
       country_code: countryCode,
-      limit: 300
+      limit: 300,
+    };
+  
+    try {
+      const response = await fetch(`/api/fasty-bot/proxy-search${builQueryString(params)}`);
+      const data = await response.json();
+      return (data.data as Region[]) || [];
+    } catch (error) {
+      console.error('Error fetching:', error);
+      return [];
     }
-    fetch(`/api/fasty-bot/proxy-search${builQueryString(params)}`)
-      .then(response => response.json())
-      .then(data => {
-        setRegionData((data.data as Region[]) || [])
-      })
-      .catch(error => {
-        console.error('Error fetching:', error)
-      })
-  }
+  };
+  
 
   const getCityList = async (regionId: string, q: string) => {
     const params = {
@@ -231,14 +270,32 @@ export function GeographicalLocation({
     return []
   }
   const searchCity = useCallback(
-    debounce(async (regionId: string, searchTerm: string) => {
+    debounce(async (index:number, regionId: string, searchTerm: string) => {
       setLoading(true)
       try {
         const cities = await getCityList(regionId, searchTerm)
-        setCityData(cities)
+        setSelectedGeoLocations((prev) =>
+          prev.map((item, i) =>
+            i === index
+              ? {
+                  ...item,
+                  cityData:cities
+                }
+              : item
+          )
+        );
       } catch (error) {
         console.error('Error fetching results:', error)
-        setCityData([])
+        setSelectedGeoLocations((prev) =>
+          prev.map((item, i) =>
+            i === index
+              ? {
+                  ...item,
+                  cityData:[]
+                }
+              : item
+          )
+        );
       } finally {
         setLoading(false)
       }
@@ -257,20 +314,56 @@ export function GeographicalLocation({
     }
   }, [countrySelected])
 
-  const handleSelectCity = (value: string) => {
-    const city = cityData.find(e => e.key === value)
-    const exist = citiesSelected.find(e => e.key === value)
+  const handleSelectCity = (index:number, value: string) => {
+    // const city = cityData.find(e => e.key === value)
+    // const exist = citiesSelected.find(e => e.key === value)
+    const city = selectedGeoLocations[index]?.cityData?.find((e) => e.key === value);
+
+    const exist = selectedGeoLocations[index]?.cities?.find((e) => e.key === value);
+  
     if (city && !exist) {
-      setCitiesSelected([...citiesSelected, city])
+      // setCitiesSelected([...citiesSelected, city])
+      setSelectedGeoLocations((prev) =>
+        prev.map((item, i) =>
+          i === index
+            ? {
+                ...item,
+                cities:[...item.cities,city],
+                cityData:[]
+              }
+            : item
+        )
+      );
+    }else{
+      setSelectedGeoLocations((prev) =>
+        prev.map((item, i) =>
+          i === index
+            ? {
+                ...item,
+                cityData:[]
+              }
+            : item
+        )
+      );
     }
     setCityData([])
   }
-  const handleRemoveCity = (value: string) => {
-    setCitiesSelected(citiesSelected.filter(city => city.key !== value))
+  const handleRemoveCity = (index:number, value: string) => {
+    // setCitiesSelected(citiesSelected.filter(city => city.key !== value))
+    setSelectedGeoLocations((prev) =>
+      prev.map((item, i) =>
+        i === index
+          ? {
+              ...item,
+              cities:item.cities.filter(city => city.key !== value)
+            }
+          : item
+      )
+    );  
   }
-  const handleChangeKeyword = (value: string) => {
+  const handleChangeKeyword = (index:number, regionSelected:Region,value: string) => {
     if (regionSelected && value.length > 0) {
-      searchCity(regionSelected.key, value)
+      searchCity(index,regionSelected.key, value)
     } else {
       setCityData([])
     }
@@ -280,6 +373,71 @@ export function GeographicalLocation({
     setAgeMin(min)
   }
 
+  const handleAddCountry= ()=>{
+    setSelectedGeoLocations((prev)=>[...prev, {
+      country:null,
+      region:null,
+      cities:[],
+      regionData:[],
+      cityData:[]
+    }])
+  }
+
+  const handleRemoveCountry = (index: number) => {
+    setSelectedGeoLocations((prev) => prev.filter((_, i) => i !== index))
+  }
+
+  const handleCountrySelect = async (index: number, country: Country|null) => {
+    let regionData:Region[]=[]
+    if(country){
+      regionData = await getRegionList(country.country_code);
+    }
+    setSelectedGeoLocations((prev) =>
+      prev.map((item, i) =>
+        i === index
+          ? {
+              ...item,
+              country, 
+              region: null, 
+              city: [],
+              regionData:regionData,
+              cityData:[]
+            }
+          : item
+      )
+    );
+  };
+
+  const handleRegionSelect = (index: number, region: Region|null) => {
+
+    setSelectedGeoLocations((prev) =>
+      prev.map((item, i) =>
+        i === index
+          ? {
+              ...item,
+              region: region, 
+            }
+          : item
+      )
+    );
+  };
+
+  // const handleCitySelect = (index: number, region: Region) => {
+  //   setSelectedGeoLocations((prev) =>
+  //     prev.map((item, i) =>
+  //       i === index
+  //         ? {
+  //             ...item,
+  //             region: region, 
+  //             city: []
+  //           }
+  //         : item
+  //     )
+  //   );
+  // };
+  
+
+
   return graphicalLocationUI ? (
     graphicalLocationUI
   ) : (
@@ -287,68 +445,103 @@ export function GeographicalLocation({
       <div className="text-lg font-medium text-gray-900 dark:text-zinc-300 mb-2">
         Let&apos;s select geographical area:
       </div>
-      <div className="mb-4">
-        <Label className="dark:text-zinc-200">Country</Label>
-        <Select
-          disabled={countryData.length === 0 || isReadOnly}
-          value={countrySelected?.country_code}
-          onValueChange={value => {
-            setCountrySelected(countryData.find(e => e.key === value))
-          }}
-        >
-          <SelectTrigger className="SelectTrigger" aria-label="Food">
-            <SelectValue placeholder="Select a country" />
-          </SelectTrigger>
-          <SelectContent>
-            {countryData.map((country: Country) => {
-              return (
-                <SelectItem key={country.key} value={country.key}>
-                  {country.name}
-                </SelectItem>
+      {
+        selectedGeoLocations.map((geoLocation, index) => (
+          <div className='border rounded p-4 mb-3'>
+           <div className="mb-4">
+              <Label className="dark:text-zinc-200">Country</Label>
+              <Select
+                disabled={countryData.length === 0 || isReadOnly}
+                value={geoLocation.country?.country_code}
+                onValueChange={value => {
+                  handleCountrySelect(index, countryData.find(e => e.key === value)??null)
+                  // setCountrySelected(countryData.find(e => e.key === value))
+                }}
+              >
+                <SelectTrigger className="SelectTrigger" aria-label="Food">
+                  <SelectValue placeholder="Select a country" />
+                </SelectTrigger>
+                <SelectContent>
+                  {countryData.map((country: Country) => {
+                    return (
+                      <SelectItem key={country.key} value={country.key}>
+                        {country.name}
+                      </SelectItem>
+                    )
+                  })}
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="mb-4">
+              <Label className="dark:text-zinc-200">Region</Label>
+              <Select
+                disabled={geoLocation.regionData.length === 0 || isReadOnly}
+                onValueChange={value => {
+                  handleRegionSelect(index,geoLocation.regionData.find(e => e.key === value)??null)
+                  // setRegionSelected(regionData.find(e => e.key === value))
+                }}
+              >
+                <SelectTrigger className="SelectTrigger" aria-label="Food">
+                  <SelectValue placeholder="Select a region" />
+                </SelectTrigger>
+                <SelectContent>
+                  {geoLocation.regionData.map((region: Region) => {
+                    return (
+                      <SelectItem key={region.key} value={region.key}>
+                        {region.name}
+                      </SelectItem>
+                    )
+                  })}
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="mb-4">
+              <Label className="dark:text-zinc-200">City</Label>
+              <ComboBox
+                disabled={!geoLocation.region || isReadOnly}
+                selectedOptions={geoLocation.cities.map(city => ({
+                  label: city.name,
+                  value: city.key
+                }))}
+                onChangeKeyword={(value:string)=>geoLocation.region&&handleChangeKeyword(index, geoLocation.region, value)}
+                options={geoLocation.cityData.map(city => ({
+                  label: city.name,
+                  value: city.key
+                }))}
+                onSelect={(value)=>handleSelectCity(index,value)}
+                onRemove={(value)=>handleRemoveCity(index, value)}
+              />
+            </div>
+            {
+              selectedGeoLocations.length>1&&(
+              <div className="">
+                <Button
+                  disabled={isSubmitting}
+                  onClick={()=>handleRemoveCountry(index)}
+                  className="flex ml-auto justify-center items-center flex-1 px-3 py-2 text-xs align-middle font-medium text-center"
+                >
+                  Remove Country
+                </Button>
+              </div>
               )
-            })}
-          </SelectContent>
-        </Select>
-      </div>
-      <div className="mb-4">
-        <Label className="dark:text-zinc-200">Region</Label>
-        <Select
-          disabled={regionData.length === 0 || isReadOnly}
-          onValueChange={value => {
-            setRegionSelected(regionData.find(e => e.key === value))
-          }}
+            }
+            
+          
+          </div>
+        ))
+      }
+
+      <div className="mt-4 mb-6">
+        <Button
+          disabled={isSubmitting}
+          onClick={handleAddCountry}
+          className="flex justify-center items-center flex-1 px-3 py-2 text-xs align-middle font-medium text-center"
         >
-          <SelectTrigger className="SelectTrigger" aria-label="Food">
-            <SelectValue placeholder="Select a region" />
-          </SelectTrigger>
-          <SelectContent>
-            {regionData.map((region: Region) => {
-              return (
-                <SelectItem key={region.key} value={region.key}>
-                  {region.name}
-                </SelectItem>
-              )
-            })}
-          </SelectContent>
-        </Select>
+          + Add Country
+        </Button>
       </div>
-      <div className="mb-4">
-        <Label className="dark:text-zinc-200">City</Label>
-        <ComboBox
-          disabled={!regionSelected || isReadOnly}
-          selectedOptions={citiesSelected.map(city => ({
-            label: city.name,
-            value: city.key
-          }))}
-          onChangeKeyword={handleChangeKeyword}
-          options={cityData.map(city => ({
-            label: city.name,
-            value: city.key
-          }))}
-          onSelect={handleSelectCity}
-          onRemove={handleRemoveCity}
-        />
-      </div>
+
+
       <div className="mb-4">
         <Label className="dark:text-zinc-200">Age Range</Label>
         <RangeSlider
