c38f42ad83ef3da5079ab2e2249b5a9fa606d59f
mydafzal <momalafzal20@gmail.com>
2024-12-10 11:32:47 +0530

    updated api call for adcreatives to include campaign id

--- app/api/fasty-bot/proxy-get-adcreatives/route.ts
+++ app/api/fasty-bot/proxy-get-adcreatives/route.ts
@@ -3,8 +3,14 @@ import { getAdCreatives } from '@/lib/api/fasty-bot/get-adcreatives'
 
 export async function GET(request: Request) {
   try {
+    const { searchParams } = new URL(request.url)
 
-    const response = await getAdCreatives()
+    const campaignId = searchParams.get('campaignId')
+    if (!campaignId){
+      return NextResponse.json({ success: false, error:"campaignId is required" }, { status: 400 })
+    }
+
+    const response = await getAdCreatives(campaignId)
 
     if (!response.ok) {
       return NextResponse.json({ success: false }, { status: response.status })
