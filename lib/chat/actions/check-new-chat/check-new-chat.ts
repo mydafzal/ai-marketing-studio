b123fb96e603b1ce2f1ababb98cb2ebf2d83f271
mydafzal <momalafzal20@gmail.com>
2025-01-28 12:43:32 +0100

    [TEQ] Refactor Check new chat

---
+++ lib/chat/actions/check-new-chat/check-new-chat.ts
@@ -0,0 +1,65 @@
+import { Message, Session } from "@/lib/types";
+import {differenceInHours} from "date-fns";
+import {sendAdminNotification} from "@/lib/api/fasty-bot/send-admin-notification";
+
+class MessageActivityValidator{
+
+    public async informAdminIfThisIsNewActivity(chatId: string, messages: Message[], session: Session | null ): Promise<boolean>{
+
+        if(session === null || session.user === undefined){
+            return false;
+        }
+
+        if(this.getIgnoredEmails().includes(session.user.email))
+        {
+            return false;
+        }
+
+        const userMessages = this.getUserMessages(messages);
+
+        if(!this.hadNoMessagesInTheLastNHours(16, userMessages))
+        {
+            return false;
+        }
+
+        await sendAdminNotification(chatId);
+        return true;
+    }
+
+    // Todo: Create a provider for this later on
+    private getIgnoredEmails() : string[] {
+        return [
+            'teo.kostelac@outlook.com',
+            'contact@reeply.net',
+            'themadnoise@gmail.com',
+            'maxnols@reeply.net',
+            'vinayak@reeply.ai',
+            'madani.farzam@gmail.com'
+        ];
+    }
+
+    private getUserMessages(messages: Message[]): Message[] {
+        return messages.filter(
+            message => message.role === 'user'
+        )
+    }
+
+    private hadNoMessagesInTheLastNHours(hours: number, messages: Message[]): boolean {
+        return messages.some(
+            message => {
+                if(message === null || message.timestamp === undefined) {
+                    return false;
+                }
+                const now = new Date();
+                const HoursTillMessage = differenceInHours(now, message.timestamp);
+
+                return HoursTillMessage < hours;
+            }
+        )
+    }
+
+}
+
+export default MessageActivityValidator;
+
+
