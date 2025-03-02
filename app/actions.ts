e41aa18801ed712a00531a146267034a88d5bfe0
mydafzal <momalafzal20@gmail.com>
2025-03-02 09:45:34 +0100

    Paddle implementation - squashed

--- app/actions.ts
+++ app/actions.ts
@@ -909,6 +909,7 @@ export async function updateChatTitle(chatSlug: string, title: string) {
     }
 }
 
+
 export async function updateFbAccountId(email: string, fbAccountId: string) {
     const session = await auth()
 
