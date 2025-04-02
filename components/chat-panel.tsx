fcb16d9ce6bdaea25653d941b51dfda4c7ce0ffd
mydafzal <momalafzal20@gmail.com>
2025-04-02 23:11:14 +0200

    Revert "fixed build issues"
    This reverts commit b5d42608b5d3a730783d9ca639f43d8ed8be3a52.

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -1,4 +1,4 @@
-import { TextPart, ImagePart, FilePart } from 'ai'
+import { TextPart, ImagePart } from 'ai'
 import { useAIState, useActions, useUIState } from 'ai/rsc'
 import { nanoid } from 'nanoid'
 import * as React from 'react'
@@ -64,9 +64,9 @@ export function ChatPanel({
   const [shareDialogOpen, setShareDialogOpen] = React.useState(false)
   const [isTaskPaletteOpen, setIsTaskPaletteOpen] = React.useState(false)
   let isUserGuideButtonEnabled = isFeatureToggleEnabled("userGuideFloatingButton")
-  const sendMessage = React.useCallback(async (message: string, userContent?: (TextPart | ImagePart | FilePart )[]) => {
+  const sendMessage = React.useCallback(async (message: string, userContent?: (TextPart | ImagePart )[]) => {
     // Optimistically add user message UI
-    setMessages((currentMessages: any) => [
+    setMessages(currentMessages => [
       ...currentMessages,
       {
         id: nanoid(),
@@ -76,7 +76,7 @@ export function ChatPanel({
 
     // Submit and get response message
     const responseMessage = await submitUserMessage(message, userContent);
-    setMessages((currentMessages: any) => [...currentMessages, responseMessage])
+    setMessages(currentMessages => [...currentMessages, responseMessage])
   }, [])
 
   const handleShowMe = (prompt: string) => {
