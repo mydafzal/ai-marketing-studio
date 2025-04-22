05a79225c76f8e23fddc052b2525fce71f938cf6
mydafzal <momalafzal20@gmail.com>
2025-04-22 16:36:11 +0200

    [TEQ] Add customer to bypass list

--- app/subscription/subscription-bypass-list.ts
+++ app/subscription/subscription-bypass-list.ts
@@ -42,5 +42,6 @@ export const subscriptionBypassList: string[] = [
     'magdalena.wanten@bonvitagroup.com',
     'jessica.rheinberger@bonvitagroup.com',
     'lena.zoller@bonvitagroup.com',
-    'ayfer.stroehm-ilhan@bonvitagroup.com'
+    'ayfer.stroehm-ilhan@bonvitagroup.com',
+    'store@drillaz-clothing.com'
 ];
