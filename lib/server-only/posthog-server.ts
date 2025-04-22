796e71c36f07e6cb580f7a2230f71e5dbd457d1c
mydafzal <momalafzal20@gmail.com>
2025-04-22 08:34:44 +0530

    replace server posthog with client side

--- lib/server-only/posthog-server.ts
+++ lib/server-only/posthog-server.ts
@@ -1,24 +1,8 @@
 // lib/server-only/posthog-server.ts
 'use server'
 
-import { PostHog } from 'posthog-node'
+import posthog from 'posthog-js'
 import { encryptEmail } from '../email-encryption'
-import { getConfig } from '@/utils/config'
-
-let serverPosthog: PostHog | null = null
-
-async function getServerPosthog(): Promise<PostHog> {
-  if (serverPosthog) return serverPosthog
-
-  const config = await getConfig()
-
-  serverPosthog = new PostHog(config.posthogApiKey, {
-    host: 'https://eu.i.posthog.com',
-    flushAt: 1
-  })
-
-  return serverPosthog
-}
 
 const INTERNAL_EMAILS = ['@reeply.ai', '@reeply.net']
 // const INTERNAL_EMAILS = ['@gmail.com']
@@ -43,14 +27,10 @@ export async function trackServerEvent({
   if (await isInternalUser(user.email)) return
 
   const encryptedEmail = await encryptEmail(user.email)
-  const posthog = await getServerPosthog()
-
-  await posthog.capture({
-    distinctId: user.id,
-    event,
-    properties: {
-      email: encryptedEmail,
-      ...properties
-    }
+  
+  posthog.capture(event, {
+    distinct_id: user.id,
+    email: encryptedEmail,
+    ...properties
   })
 }
