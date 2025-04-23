cf57a736f9fda6c311c9c57d1f6bf00d8b43cbb5
mydafzal <momalafzal20@gmail.com>
2025-04-23 19:30:26 +0200

    TS eerrorr

--- components/chat.tsx
+++ components/chat.tsx
@@ -356,7 +356,7 @@ function ChatCore({ id, chat, className, session, missingKeys }: ChatProps) {
 
     if (messages.length > 0) {
       // Filter out any message marked as hidden before passing to ChatList
-      const visibleMessages = messages.filter(message => !message.hidden);
+      const visibleMessages = messages.filter(message => typeof message === 'object' && 'hidden' in message ? !message.hidden : true);
       return <ChatList messages={visibleMessages} isShared={false} session={session} />
     }
 
