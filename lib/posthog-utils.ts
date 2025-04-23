1a0326797fea5549401e32defd93ab1682ed0673
mydafzal <momalafzal20@gmail.com>
2025-04-23 18:31:35 +0200

    add comment

--- lib/posthog-utils.ts
+++ lib/posthog-utils.ts
@@ -2,7 +2,7 @@
 const INTERNAL_EMAILS = ['@reeply.ai', '@reeply.net']
 
 export function isInternalUser(email?: string): boolean {
-    if (email === 'vinayak@reeply.ai') return false;
+    if (email === 'vinayak@reeply.ai') return false; // this account is used for fb verification so tracking should be enabled on it
     if (!email) return false
     return INTERNAL_EMAILS.some((domain) => email.toLowerCase().includes(domain))
 }
