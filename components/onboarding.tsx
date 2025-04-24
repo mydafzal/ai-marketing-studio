b839a52403f7d358d7800a61453b4cb30636a740
mydafzal <momalafzal20@gmail.com>
2025-04-24 15:22:27 +0200

    [TEQ] Add Portuguese to preferred languages

--- components/onboarding.tsx
+++ components/onboarding.tsx
@@ -875,6 +875,7 @@ function Onboarding({
                             <option value="es">Spanish</option>
                             <option value="it">Italian</option>
                             <option value="fr">French</option>
+                            <option value="pt">Portuguese</option>
                         </select>
                         {fieldError && (
                             <p className="text-sm text-red-500 flex items-center gap-1">
