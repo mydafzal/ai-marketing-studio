cc813fa6fa17ccdc45f15edf6e2fabee6c80326a
mydafzal <momalafzal20@gmail.com>
2025-04-26 16:25:21 +0530

    track event integrated for subscription analysis

--- app/api/subscription/route.ts
+++ app/api/subscription/route.ts
@@ -1,6 +1,5 @@
 import { subscriptionUpdated, subscriptionCreated, subscriptionCanceled } from "@/app/paddle";
 import { NextRequest, NextResponse } from "next/server";
-import { trackEvent } from "@/lib/utils";
 
 export async function POST(req: NextRequest) {
 	try {
@@ -12,38 +11,14 @@ export async function POST(req: NextRequest) {
 		if (response.event_type === "subscription.updated") {
 			console.log(`Subscription ${response.data.id} renewed.`);
 			await subscriptionUpdated(response.data);
-			await trackEvent('Subscription Updated', 
-				{ email: response.data.customer.email, id: response.data.customer.id },
-				{ 
-					subscription_id: response.data.id,
-					plan_id: response.data.items[0].price_id,
-					status: response.data.status
-				}
-			);
 		}
 		else if (response.event_type === "subscription.created") {
 			console.log(`Subscription ${response.data.id} created.`);
 			await subscriptionCreated(response.data);
-			await trackEvent('Subscription Created', 
-				{ email: response.data.customer.email, id: response.data.customer.id },
-				{ 
-					subscription_id: response.data.id,
-					plan_id: response.data.items[0].price_id,
-					status: response.data.status
-				}
-			);
 		}
 		else if (response.event_type === "subscription.canceled") {
 			console.log(`Subscription ${response.data.id} cancelled.`);
 			await subscriptionCanceled(response.data);
-			await trackEvent('Subscription Canceled', 
-				{ email: response.data.customer.email, id: response.data.customer.id },
-				{ 
-					subscription_id: response.data.id,
-					plan_id: response.data.items[0].price_id,
-					status: response.data.status
-				}
-			);
 		}
 		return NextResponse.json({ success: true, message: "Webhook received" });
 	} catch (error) {
