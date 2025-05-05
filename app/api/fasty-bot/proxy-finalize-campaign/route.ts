6b3cb6091ee709490f7beb3d75b7e95370a3a3e8
mydafzal <momalafzal20@gmail.com>
2025-05-05 14:09:16 +0200

    [TEQ] Campaign Publish Config

--- app/api/fasty-bot/proxy-finalize-campaign/route.ts
+++ app/api/fasty-bot/proxy-finalize-campaign/route.ts
@@ -77,10 +77,16 @@ export async function POST(req: NextRequest) {
     });
     
     // Prepare the request payload with the correctly formatted FB Account ID
+
+    let config = {
+      "publish_campaign_after_creation": true
+    }
+
     const requestPayload = {
       fb_account_id: formattedFbAccountId,
       campaign_flow_session_id,
-      page_id
+      page_id,
+      config: config
     };
     
     // Prepare headers - for admin-assigned accounts, don't send any token
