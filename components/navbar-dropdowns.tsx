95927a2072b30f008b441967289c3df3730a99cf
mydafzal <momalafzal20@gmail.com>
2025-05-08 12:07:51 +0200

    add tooltip

--- components/navbar-dropdowns.tsx
+++ components/navbar-dropdowns.tsx
@@ -476,9 +476,9 @@ const NavbarDropdowns = ({
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5 text-amber-400 cursor-help" viewBox="0 0 20 20" fill="currentColor">
                   <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
                 </svg>
-                <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-64 bg-black text-white text-xs rounded py-2 px-3 shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none z-50">
+                <div className="absolute top-full left-0 mt-2 w-64 bg-black text-white text-xs rounded py-2 px-3 shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none z-50">
                   To connect your Instagram account, please configure it in your Facebook Business Manager. Need help? Use our support chat widget to get in touch.
-                  <div className="absolute top-full left-1/2 transform -translate-x-1/2 -mt-1 border-t-4 border-black"></div>
+                  <div className="absolute bottom-full left-2 transform -translate-x-1/2 -mb-1 border-b-4 border-black"></div>
                 </div>
               </div>
             </div>
@@ -561,9 +561,9 @@ const NavbarDropdowns = ({
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5 text-amber-400 cursor-help" viewBox="0 0 20 20" fill="currentColor">
                   <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
                 </svg>
-                <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-64 bg-black text-white text-xs rounded py-2 px-3 shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none z-50">
+                <div className="absolute top-full left-0 mt-2 w-64 bg-black text-white text-xs rounded py-2 px-3 shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none z-50">
                   To connect your Instagram account, please configure it in your Facebook Business Manager. Need help? Use our support chat widget to get in touch.
-                  <div className="absolute top-full left-1/2 transform -translate-x-1/2 -mt-1 border-t-4 border-black"></div>
+                  <div className="absolute bottom-full left-2 transform -translate-x-1/2 -mb-1 border-b-4 border-black"></div>
                 </div>
               </div>
             </div>
